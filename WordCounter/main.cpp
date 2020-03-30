8ce56fabc8ad5867794dad14645f5e5b90a11a79
Centurion256 <Vkuchynskyy@gmail.com>
2020-03-30 18:35:43 +0300

    Update cmake

--- WordCounter/main.cpp
+++ WordCounter/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <thread>
+#include <mutex>
+//#include <functional>
+#define LIMIT 10000
+// #include <any>
+
+void print_smth(int x, std::mutex& m)
+{
+    for (int i = 0; i < LIMIT; i++)
+    {
+        std::lock_guard<std::mutex> lg{m};
+        std::cout << "hello from a thread!, num =" << x << std::endl;
+    }
+}
 
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    
+    std::mutex m1;
+    std::thread t1{print_smth, 2, std::ref(m1)};
+    std::thread t2{print_smth, 100, std::ref(m1)};
+    for (int i = 0; i < LIMIT; i++)
+    {
+        std::lock_guard<std::mutex> lg{m1};
+        std::cout << "Hello, world!\n";
+    }
+    t1.join();
+    t2.join();
     return 0;
 }
